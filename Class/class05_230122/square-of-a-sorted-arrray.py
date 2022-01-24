fa33f53581a59a74ce625bf1df2b7d17386bea82
roshan.kumar <roshan.kumar@scientificgames.com>
2022-01-24 12:25:33 +0530

    working sol of sortedSquare problem

--- Class/class05_230122/square-of-a-sorted-arrray.py
+++ Class/class05_230122/square-of-a-sorted-arrray.py
@@ -1,29 +1,29 @@
+# def sorted_square(nums):
+#     n=len(nums)
+#     j=n-1
+#     for i in range(n):
+#         nums[i]=nums[i]*nums[i]
+#
+#     while(j>0):
+#         print(nums)
+#         if(nums[0]>nums[j]):
+#             tmp=nums[0]
+#             nums[0]=nums[j]
+#             nums[j]=tmp
+#         j-=1
+#
+#     return nums
+#
+#
+# nums=[-4,-1,0,3,10]
+# print(sorted_square(nums))
+#
+#
+# =============================================================
+#
+# class Solution:
 def sorted_square(nums):
     n=len(nums)
-    j=n-1
-    for i in range(n):
-        nums[i]=nums[i]*nums[i]
-
-    while(j>0):
-        print(nums)
-        if(nums[0]>nums[j]):
-            tmp=nums[0]
-            nums[0]=nums[j]
-            nums[j]=tmp
-        j-=1
-
-    return nums
-
-
-nums=[-4,-1,0,3,10]
-print(sorted_square(nums))
-
-
-=============================================================
-
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        n=len(nums)
 
 #         #Approach1
 #         j=n-1
@@ -40,22 +40,26 @@ class Solution:
 
 #         return nums
 
-        #Approach2
-        low=0
-        high=n-1
-        res=[None]*n
-        idx=n-1
-        while low<high:
-            lowSq=nums[low]*nums[low]
-            highSq=nums[high]*nums[high]
-            if lowSq<highSq:
-                print(idx)
-                res[idx]=highSq
-                high-=1
-                idx-=1
-            else:
-                print(idx)
-                res[idx]=lowSq
-                low+=1
-                idx-=1
-        return res
+    #Approach2
+    low=0
+    high=n-1
+    res=[None]*n
+    idx=n-1
+    while low<=high:
+        lowSq=nums[low]*nums[low]
+        highSq=nums[high]*nums[high]
+        if lowSq<highSq:
+            print(idx)
+            res[idx]=highSq
+            high-=1
+            idx-=1
+        else:
+            print(idx)
+            res[idx]=lowSq
+            low+=1
+            idx-=1
+    return res
+
+
+nums=[-5,-4,-3,-1,0,3,10]
+print(sorted_square(nums))
