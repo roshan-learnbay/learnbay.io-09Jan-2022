39d8949f65901c77332a86a76200997e96572108
roshan.kumar <roshan.kumar@scientificgames.com>
2022-02-08 10:27:02 +0530

    effient approach of anagrams without sorting

---
+++ Class/class07_300122/permutation-in-string.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isZero(self,freqCount):
+        for i in range(26):
+            if freqCount[i]!=0:
+                return False
+        return True
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+#******************************************************
+#       Working but O(m*nlogn)
+#******************************************************
+        '''
+            n=len(s2)
+            start=0
+            end=len(s1)-1
+            s1 = ''.join(sorted(s1))
+            while(end<n):
+                tmpStr=str(s2[start:end+1])
+                tmpStr = ''.join(sorted(tmpStr))
+                if tmpStr==s1:
+                    return True
+                start+=1
+                end+=1
+            return False
+        '''
+#******************************************************
+#       Working with O(N)
+#******************************************************
+        n=len(s2)
+        freq=[0]*26
+        for c in s1:
+            freq[ord(c)-ord('a')]+=1
+        #print(freq)
+        start=end=0
+        while end<n:
+            freq[ord(s2[end])-ord('a')]-=1
+
+            while (end-start+1)>len(s1):
+                freq[ord(s2[start])-ord('a')]+=1
+                start+=1
+            if (end-start+1)==len(s1):
+                if self.isZero(freq):
+                    return True
+            #print(freq)
+            end+=1
+        return False
